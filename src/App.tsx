3affa5d1bab0d0cad9b254798a513474e09b9eee
zecamilplano <ezequiasgama2@gmail.com>
2024-11-03 13:53:28 -0300

    Tailwind adicionado

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import "./index.css"
 function App() {
 
   return (
-    <>
-      <h1>Hello world!</h1>
-    </>
+    <div >
+      <h1 className="text-xl font-bold underline">Hello world!</h1>
+    </div>
   )
 }
 

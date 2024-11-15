03c58a8db98f0648ae7216f41203849db1e750d1
zecamilplano <ezequiasgama2@gmail.com>
2024-11-14 22:53:42 -0300

    feat(home): Implementar seção home

--- src/page/home.tsx
+++ src/page/home.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../components/default/header";
+import { MainHome } from "../components/home/main";
 
 export function Home() {
   return (
-    <>
+    <div className="w-screen h-screen flex flex-col">
       <Header />
-    </>
+      <MainHome />
+    </div>
   )
 }

37b6792a65eaea82346e79fbb801a8fd9db7e932
zecamilplano <ezequiasgama2@gmail.com>
2025-02-03 15:05:14 -0300

    feat(products): página de produtos adicionada

---
+++ src/app/(pages)/products/card-product.tsx
@@ -0,0 +1,26 @@
+"use client"
+import { useProduct } from "@/app/hooks/hook"
+import Image from "next/image"
+
+export function CardProduct() {
+  const { products } = useProduct()
+
+  return (
+    <div className="flex flex-row flex-wrap justify-center gap-5 gap-y-0 md:pb-1">
+      <div className="bg-main h-96 overflow-scroll pt-14 font-sans flex flex-row items-center gap-9 ">
+        {products.map((product) => (
+          <article key={product.id} className="bg-white w-80 flex flex-col flex-wrap text-center mt-5 justify-center">
+            <Image
+              src={product.srcProduct}
+              alt={`produto ${product.nameProduct}`}
+              width={176}
+              height={176}
+              className="w-44 flex self-center" />
+            <h2 className="text-xl font-medium text-card-product">{product.nameProduct}</h2>
+            <button className="bg-button mx-3 rounded text-white text-xl py-2 hover:opacity-70 active:opacity-50 mb-3 outline-none">{product.textButton}</button>
+          </article>
+        ))}
+      </div>
+    </div >
+  )
+}
---
+++ src/app/(pages)/products/menu-category.tsx
@@ -0,0 +1,15 @@
+export function MenuResidentialCommercial() {
+  return (
+    <div className="w-[90%] bg-gray px-4 flex justify-self-center gap-9  ">
+      <section className="bg-white w-1/2 text-center py-2 my-2">
+        <button className="w-auto font-sans">Residênciais</button>
+      </section>
+
+      <section className="w-1/2 text-center py-2 my-2">
+        <button className="text-menu-product font-sans">Comerciais</button>
+      </section>
+    </div>
+
+  )
+}
+

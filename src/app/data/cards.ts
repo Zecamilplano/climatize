c4665e7fad400290263e6d301dc245d597d6b81f
zecamilplano <ezequiasgama2@gmail.com>
2025-03-29 12:51:26 -0300

    array de informações de cards de adicionar novos produtos

---
+++ src/app/data/cards.ts
@@ -0,0 +1,17 @@
+export const listCard = {
+  featuresMain: {
+    titleInformation: "Características Principais",
+    listInformation: [],
+    addNewInformationPlaceholder: "Adicionar novas características"
+  },
+  benefits: {
+    titleInformation: "Benefícios",
+    listInformation: [],
+    addNewInformationPlaceholder: "Adicionar novos benefícios"
+  },
+  technicalSpecification: {
+    titleInformation: "Especificações",
+    listInformation: [],
+    addNewInformationPlaceholder: "Adicionar novos especificação"
+  }
+}

4355fdcde5f2cd9f97c6e7e7e672a086c1999d81
zecamilplano <ezequiasgama2@gmail.com>
2025-04-07 14:01:56 -0300

    adicionado o header do login e da pagina de admin

--- src/app/(components)/(header)/header.tsx
+++ src/app/(components)/(header)/header.tsx
@@ -2,15 +2,20 @@
 import { usePathname } from "next/navigation"
 import { MainHeader } from "./main-header"
 import { ProductHeader } from "./product-header"
+import { LoginHeader } from "./login-header"
 
 export function Header() {
 
   const pathname = usePathname()
   const isHomeRoute = pathname.startsWith("/inicio")
   const isProductsRoute = pathname.startsWith("/produtos")
+  const isLoginRoute = pathname.startsWith("/entrar")
+  const isAdminRoute = pathname.startsWith("/painel-admin")
 
   if (isHomeRoute) return <MainHeader />
   if (isProductsRoute) return <ProductHeader />
+  if (isLoginRoute) return <LoginHeader />
+  if (isAdminRoute) return <LoginHeader />
 
   return (
     <MainHeader />
---
+++ src/app/(components)/(header)/login-header.tsx
@@ -0,0 +1,14 @@
+import { House } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+export function LoginHeader() {
+  return (
+    <header className="bg-white flex flex-col md:flex-row justify-between items-center md:px-1 py-2 shadow-header absolute left-0 right-0 top-0 ">
+      <Image src="/logo.png" width={200} height={200} alt="Logo" />
+      <nav>
+        <Link href="/" className="hidden font-inter md:flex items-center text-2xl gap-1"><House width={20} height={20} /> Início</Link>
+      </nav>
+    </header>
+  )
+}

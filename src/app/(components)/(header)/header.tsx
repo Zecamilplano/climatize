84bb9cba3497f39e842714c19dc28491165e7ec6
zecamilplano <ezequiasgama2@gmail.com>
2025-04-28 19:16:35 -0300

    feat(header): adicionado novo header para a tela de produto

--- src/app/(components)/(header)/header.tsx
+++ src/app/(components)/(header)/header.tsx
@@ -3,6 +3,7 @@ import { usePathname } from "next/navigation"
 import { MainHeader } from "./main-header"
 import { ProductHeader } from "./product-header"
 import { LoginHeader } from "./login-header"
+import { NewProductHeader } from "./new-product-header"
 
 export function Header() {
 
@@ -11,11 +12,13 @@ export function Header() {
   const isProductsRoute = pathname.startsWith("/produtos")
   const isLoginRoute = pathname.startsWith("/entrar")
   const isAdminRoute = pathname.startsWith("/painel-admin")
+  const isNewProductRoute = pathname.startsWith("/novo-produto")
 
   if (isHomeRoute) return <MainHeader />
   if (isProductsRoute) return <ProductHeader />
   if (isLoginRoute) return <LoginHeader />
   if (isAdminRoute) return <LoginHeader />
+  if (isNewProductRoute) return <NewProductHeader />
 
   return (
     <MainHeader />
